bd64162d76fe57050c685e49de7df2a29b71411a
Raul Ulrich Matarazo <raul.ulrich.matarazo@gmail.com>
2024-04-16 08:32:39 -0300

    Update EscolaApiApplication.java

--- src/main/java/com/api/escolaapi/EscolaApiApplication.java
+++ src/main/java/com/api/escolaapi/EscolaApiApplication.java
@@ -8,6 +8,7 @@ public class EscolaApiApplication {
     public static void main(String[] args) {
         SpringApplication.run(EscolaApiApplication.class, args);
         System.out.println("Aplicação está rodando...");
+        System.out.println("Mudando algo...");
         System.out.println("Porta: 8080");
     }
 }
