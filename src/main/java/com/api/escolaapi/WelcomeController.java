625dd28c5d36ae76397286563a4a9e2c1a76ef10
Raul Ulrich Matarazo <raul.ulrich.matarazo@gmail.com>
2024-05-18 11:24:12 -0300

    updates

--- src/main/java/com/api/escolaapi/WelcomeController.java
+++ src/main/java/com/api/escolaapi/WelcomeController.java
@@ -2,14 +2,16 @@ package com.api.escolaapi;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @Controller
-@RequestMapping(path = "/")
 public class WelcomeController {
-    @GetMapping
+    @GetMapping("/")
     public String showWelcomeTemplate(){
         return "welcome";
+    }
+
+    @GetMapping("/cadastrar-aluno")
+    public String showCreateNewStudentTemplate(){
+        return "cadastrarAluno";
     }
 }
