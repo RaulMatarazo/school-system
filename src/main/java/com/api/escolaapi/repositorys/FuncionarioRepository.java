19ae37de00a0d6516f4d32102d6523a54f8b99c8
Raul Ulrich Matarazo <raul.ulrich.matarazo@gmail.com>
2024-08-17 17:55:43 -0300

    Updates

--- src/main/java/com/api/escolaapi/repositorys/FuncionarioRepository.java
+++ src/main/java/com/api/escolaapi/repositorys/FuncionarioRepository.java
@@ -4,11 +4,11 @@ import com.api.escolaapi.funcionario.FuncionarioClass;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface FuncionarioRepository extends JpaRepository<FuncionarioClass, Integer> {
     List<FuncionarioClass> findAllByAtivoTrue();
     List<FuncionarioClass> findAllByAtivoFalse();
     List<FuncionarioClass> findByEmail(String email);
     List<FuncionarioClass> findByTelefone(String telefones);
-
 }
