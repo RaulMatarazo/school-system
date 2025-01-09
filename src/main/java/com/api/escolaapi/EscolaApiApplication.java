9b08fa16be9f87eea366bf0737b95cb62a64c75e
Raul Ulrich Matarazo <raul.ulrich.matarazo@gmail.com>
2025-01-09 19:18:50 -0300

    Update EscolaApiApplication.java

--- src/main/java/com/api/escolaapi/EscolaApiApplication.java
+++ src/main/java/com/api/escolaapi/EscolaApiApplication.java
@@ -8,7 +8,7 @@ public class EscolaApiApplication {
     public static void main(String[] args) {
         SpringApplication.run(EscolaApiApplication.class, args);
         System.out.println("A aplicação está rodando...");
-        System.out.println("Clique aqui para acessar o projeto: http://localhost:8080/login");
+        System.out.println("Clique aqui para acessar o projeto: http://localhost:8080/");
         System.out.println("Feito por Raul U. Matarazo");
     }
 }
